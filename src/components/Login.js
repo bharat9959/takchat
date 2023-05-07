a33a5520261572f5784fc97bd5d4ffec980f7f28
venkineo9 <venkatesh.bommu@neobric.com>
2023-05-07 19:55:09 +0530

    new app

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+function Login() {
+  return (
+    <div className="m-5">
+ <Container>
+        <Row className="justify-content-md-center">
+            <Col xs={6} className="bg-light p-5 rounded shadow-sm">
+            <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+            </Col>
+        </Row>
+    </Container>
+    </div>
+   
+   
+  );
+}
+
+export default Login;
